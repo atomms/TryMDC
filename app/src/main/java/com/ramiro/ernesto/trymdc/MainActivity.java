792690a5535c07066d56063d795c0a5243b2fae9
Ernesto Ramiro <atomms@gmail.com>
2021-01-26 12:18:41 +0100

    bottombar full interaction

--- app/src/main/java/com/ramiro/ernesto/trymdc/MainActivity.java
+++ app/src/main/java/com/ramiro/ernesto/trymdc/MainActivity.java
@@ -1,12 +1,15 @@
 package com.ramiro.ernesto.trymdc;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,27 +18,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
+        View sheet = findViewById(R.id.view);
 
-        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+
+
+
+        //click event over FAB
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Back clicked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "FAB Clicked.", Toast.LENGTH_SHORT).show();
             }
         });
 
-//        bottomAppBar.setOnMenuItemClickListener() { menuItem ->
-//                when (menuItem.itemId) {
-//            R.id.search -> {
-//                // Handle search icon press
-//                true
-//            }
-//            R.id.more -> {
-//                // Handle more item (inside overflow menu) press
-//                true
-//            }
-//        else -> false
-//        }
-//        }
+        //click event over Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Menu clicked!", Toast.LENGTH_SHORT).show();
+            }
+
+//            //open bottom sheet
+//                    BottomSheetDialogFragment bottomSheetDialogFragment = BottomSheetNavigationFragment.newInstance();
+//                bottomSheetDialogFragment.show(getSupportFragmentManager(), "Bottom Sheet Dialog Fragment");
+//                    sheet.bottomSheetBehavior.setPeekHeight(300);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        });
+
+
+        //click event over Bottom bar menu item
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.more:
+                        Toast.makeText(MainActivity.this, "More clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.search:
+                        Toast.makeText(MainActivity.this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+
 
     }
 
