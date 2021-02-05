6f52931dbddf79922b10726149e0ced2e048eb52
Ernesto Ramiro <atomms@gmail.com>
2021-02-05 08:14:27 +0100

    new shiny and frisky bottomappbar

---
+++ app/src/main/java/com/ramiro/ernesto/trymdc/ItemViewModel.java
@@ -0,0 +1,21 @@
+package com.ramiro.ernesto.trymdc;
+
+import android.content.ClipData;
+import android.view.MenuItem;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ItemViewModel extends ViewModel {
+    private final MutableLiveData<ClipData.Item> selectedItem = new MutableLiveData<ClipData.Item>();
+    public void selectItem(ClipData.Item item) {
+        selectedItem.setValue(item);
+    }
+    public LiveData<ClipData.Item> getSelectedItem() {
+        return selectedItem;
+    }
+
+    public void select(MenuItem item) {
+    }
+}
--- app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
+++ app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +21,7 @@ import android.widget.Toast;
 import com.ramiro.ernesto.trymdc.ui.main.SectionsPagerAdapter;
 
 public class TabbedActivity extends AppCompatActivity {
-
+    private ItemViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +31,12 @@ public class TabbedActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        //startFragment
+        viewModel = new ViewModelProvider(this).get(ItemViewModel.class);
+        viewModel.getSelectedItem().observe(this, item -> {
+            // Perform an action with the latest item data
+        });
 
 // cast al xml
         BottomNavigationView bottomNavBar = findViewById(R.id.bottom_navigation);
@@ -42,6 +49,7 @@ bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavi
             case R.id.likes:
                 item.setChecked(true);
                 Toast.makeText(TabbedActivity.this, "Likes clicked.", Toast.LENGTH_SHORT).show();
+                viewModel.select(item);
             break;
             case R.id.add:
                 item.setChecked(true);
