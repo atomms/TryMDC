713096ff462305ad2d88a9d32e82aee37415400a
Ernesto Ramiro <atomms@gmail.com>
2021-02-10 12:13:51 +0100

    bottom nav moving tabs

--- app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
+++ app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
@@ -18,10 +18,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.ramiro.ernesto.trymdc.ui.main.PlaceholderFragment;
 import com.ramiro.ernesto.trymdc.ui.main.SectionsPagerAdapter;
 
 public class TabbedActivity extends AppCompatActivity {
     private ItemViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,36 +31,48 @@ public class TabbedActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+//        TabLayout tabs = findViewById(R.id.tabs);
+//        tabs.setupWithViewPager(viewPager);
 
         //startFragment
         viewModel = new ViewModelProvider(this).get(ItemViewModel.class);
         viewModel.getSelectedItem().observe(this, item -> {
             // Perform an action with the latest item data
+
         });
 
 // cast al xml
         BottomNavigationView bottomNavBar = findViewById(R.id.bottom_navigation);
 
-bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.likes:
-                item.setChecked(true);
-                Toast.makeText(TabbedActivity.this, "Likes clicked.", Toast.LENGTH_SHORT).show();
-                viewModel.select(item);
-            break;
-            case R.id.add:
-                item.setChecked(true);
-                Toast.makeText(TabbedActivity.this, "Add clicked.", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return false;
-    }
-});
+                switch (item.getItemId()) {
+                    case R.id.likes:
+                        item.setChecked(true);
+                        Toast.makeText(TabbedActivity.this, "Likes clicked.", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.add:
+                        item.setChecked(true);
+                        Toast.makeText(TabbedActivity.this, "Add clicked.", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.profile:
+                        item.setChecked(true);
+                        Toast.makeText(TabbedActivity.this, "Add clicked.", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.marker:
+                        item.setChecked(true);
+                        Toast.makeText(TabbedActivity.this, "Add clicked.", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(3);
+                        break;
+                }
+                return false;
+            }
+        });
 
     }
 }
