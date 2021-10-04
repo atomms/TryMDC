46a88db7f83c354c067f3583194b534dc0b947b9
Ernesto Ramiro <atomms@gmail.com>
2021-10-04 09:52:21 +0200

    themes and tabbed

--- app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
+++ app/src/main/java/com/ramiro/ernesto/trymdc/TabbedActivity.java
@@ -1,28 +1,22 @@
 package com.ramiro.ernesto.trymdc;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.ramiro.ernesto.trymdc.ui.main.PlaceholderFragment;
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ramiro.ernesto.trymdc.ui.main.SectionsPagerAdapter;
 
 public class TabbedActivity extends AppCompatActivity {
@@ -36,6 +30,8 @@ public class TabbedActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
+
+
 //        TabLayout tabs = findViewById(R.id.tabs);
 //        tabs.setupWithViewPager(viewPager);
 
@@ -84,6 +80,7 @@ public class TabbedActivity extends AppCompatActivity {
                         Toast.makeText(TabbedActivity.this, "Add clicked.", Toast.LENGTH_SHORT).show();
                         removeBadge(bottomNavBar,item.getItemId());
                         viewPager.setCurrentItem(2);
+
                         break;
                     case R.id.marker:
                         item.setChecked(true);
